de2f5ef38cb78761f0bf4c9ec494dc0cb61a3667
grzegorz <ggruszczynski@gmail.com>
2022-02-23 20:56:38 +0100

    rename

---
+++ to_pdf_1by1.sh
@@ -0,0 +1,28 @@
+# #!/bin/bash
+# set -e
+
+
+# this script converts a list of jupyter_notebooks.ipynb to pdf
+
+OUTPUT_DIR="pdfy"
+
+#CONVERT_OPTION include ['PDFviaHTML', 'asciidoc', 'custom', 'html', 'latex', 'markdown', 'notebook', 'pdf', 'pdfviahtml', 'python', 'rst', 'script', 'slides', 'webpdf']
+CONVERT_OPTION="PDFviaHTML" # simple pdf is not enough Oo
+
+NOTEBOOK_PATHS=(
+    "10_intro_setup" 
+    "20_vector_add"
+    "30_element_wise_matrix_add"
+    "40_parallel_reduction"
+    "50_thrust")
+
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+for i in ${!NOTEBOOK_PATHS[@]};
+do
+    NOTEBOOK=${NOTEBOOK_PATHS[$i]}
+    echo "parsing notebook[$i]: ${NOTEBOOK}.ipynb"
+    jupyter-nbconvert --to ${CONVERT_OPTION} ${NOTEBOOK}.ipynb 
+    mv ${NOTEBOOK}.pdf ${OUTPUT_DIR}/
+done
